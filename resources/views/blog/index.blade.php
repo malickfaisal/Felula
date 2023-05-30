8f132110923895e3f2cfcf9d1475254c6e0ea20a
Malik Faisal <malikfaisal544@gmail.com>
2023-05-30 14:11:55 +0100

    add rss import functionality

--- resources/views/blog/index.blade.php
+++ resources/views/blog/index.blade.php
@@ -4,7 +4,8 @@
             <h2 class="font-semibold text-xl text-gray-800 leading-tight">
                 {{ 'Blogs' }}
             </h2>
-            <a href="{{ route('blogs.create') }}" class="bg-current px-4 py-2 rounded-md">ADD BLOG</a>
+            <a href="{{ route('blogs.create') }}" class="bg-current px-4 py-2 rounded-md">Add Blogs</a>
+            <a href="{{ route('blogs.import.rss') }}" class="bg-current px-4 py-2 rounded-md">Import RSS</a>
         </div>
     </x-slot>
 
@@ -18,8 +19,7 @@
                                 <th class="border-b font-medium p-4 pl-8 pt-0 pb-3 text-slate-400 text-left">Title</th>
                                 <th class="border-b font-medium p-4 pl-8 pt-0 pb-3 text-slate-400 text-left">Slug</th>
                                 <th class="border-b font-medium p-4 pl-8 pt-0 pb-3 text-slate-400 text-left">Created At</th>
-                                <th class="border-b font-medium p-4 pl-8 pt-0 pb-3 text-slate-400 text-left">Updated At</th>
-                                <th class="border-b font-medium p-4 pl-8 pt-0 pb-3 text-slate-400 text-left">Action</th>
+                                <th class="border-b font-medium p-4 pl-8 pt-0 pb-3 text-slate-400 text-left" style="width:24%;">Action</th>
                             </tr>
                         </thead>
                         <tbody class="bg-white">
@@ -30,7 +30,6 @@
                                     <td class="border-b border-slate-100 dark:border-slate-700 p-4 pl-8 text-slate-500 dark:text-slate-400">{{ $blog->title }}</td>
                                     <td class="border-b border-slate-100 dark:border-slate-700 p-4 pl-8 text-slate-500 dark:text-slate-400">{{ $blog->slug }}</td>
                                     <td class="border-b border-slate-100 dark:border-slate-700 p-4 pl-8 text-slate-500 dark:text-slate-400">{{ $blog->created_at }}</td>
-                                    <td class="border-b border-slate-100 dark:border-slate-700 p-4 pl-8 text-slate-500 dark:text-slate-400">{{ $blog->updated_at }}</td>
                                     <td class="border-b border-slate-100 dark:border-slate-700 p-4 pl-8 text-slate-500 dark:text-slate-400">
                                         <a href="{{ route('blogs.show', $blog->id) }}" class="border border-blue-500 hover:bg-blue-500 hover:text-white px-4 py-2 rounded-md">SHOW</a>
                                         <a href="{{ route('blogs.edit', $blog->id) }}" class="border bg-current px-4 py-2 rounded-md">EDIT</a>
--- resources/views/front/blog_page.blade.php
+++ resources/views/front/blog_page.blade.php
@@ -6,8 +6,8 @@
         <div class="scale-100 p-6 bg-white dark:bg-gray-800/50 dark:bg-gradient-to-bl from-gray-700/50 via-transparent dark:ring-1 dark:ring-inset dark:ring-white/5 rounded-lg shadow-2xl shadow-gray-500/20 dark:shadow-none flex motion-safe:hover:scale-[1.01] transition-all duration-250 focus:outline focus:outline-2 focus:outline-red-500">
             <div  class="blog_div">
                 @if(isset($blog->featured_image))
-                <div class="bg-red-50 dark:bg-red-800/20 flex items-center justify-center rounded-full">                                
-                <img id="featured_image_preview" style="width:100%" class="img object-cover rounded-md" src="{{ isset($blog) ? Storage::url($blog->featured_image) : url('/images/no-preview.jpeg') }}" alt="Featured image preview" />
+                <div class="bg-red-50 dark:bg-red-800/20 flex items-center justify-center "style="width:100%">                                
+                <img id="featured_image_preview"  class="img object-cover rounded-md" src="{{ $blog->featured_image }}" alt="Featured image preview" />
                 </div>
                 @endif
                 <h1 class="mt-6 text-xl font-semibold text-gray-900 dark:text-white">{{$blog->title}}</h1>
